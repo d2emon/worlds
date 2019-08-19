5b2e8b0c5794b98c599233ce1d51fb3cb2790214
Dmitry Kutsenko <d2emonium@gmail.com>
2019-08-19 22:56:12 +0300

    Blood

--- worlds-server/app/walk/views.py
+++ worlds-server/app/walk/views.py
@@ -188,26 +188,6 @@ def kill(victim=None, weapon=None):
     )
 
 
-@blueprint.route('/look/in/', methods=['GET'])
-@blueprint.route('/look/into/', methods=['GET'])
-@blueprint.route('/look/in/<word>', methods=['GET'])
-@blueprint.route('/look/into/<word>', methods=['GET'])
-def look_in(word=None):
-    if word is None:
-        return jsonify({'error': "In what?"})
-
-    # a = fobna(word)
-    # if a is None:
-    #   return jsonify({'error': "What?"})
-    # if not a.bit[14]:
-    #   return jsonify({'error': "That isn't a container"})
-    # if a.bit[2] and a.state != 0:
-    #   return jsonify({'error': "It's closed!"})
-    # bprintf("The %s contains:\n",oname(a))
-    # aobjsat(a,3)
-    return jsonify({'result': 'Ok'})
-
-
 @blueprint.route('/exits', methods=['GET'])
 def exits():
     return do_action(Player.player().list_exits)
