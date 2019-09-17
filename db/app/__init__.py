46d98cf271eab10d19aa337fcacbe47f6ddb762f
Dmitry Kutsenko <d2emonium@gmail.com>
2019-09-17 16:23:37 +0300

    Authorization

--- db/app/__init__.py
+++ db/app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 # instantiate the app
@@ -10,7 +11,12 @@ app.config.from_object(Config)
 
 # Modules
 CORS(app)
+
+login = LoginManager(app)
+login.login_view = 'login'
+
 db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 
 from app import routes, models
--- db/app/models/post.py
+++ db/app/models/post.py
@@ -3,10 +3,10 @@ from datetime import datetime
 
 
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
