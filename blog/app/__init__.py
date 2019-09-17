ec8f5aefbe5c94265481562f301c60f17d2cda5b
Dmitry Kutsenko <d2emonium@gmail.com>
2019-09-17 12:35:29 +0300

    WTForms

--- blog/app/__init__.py
+++ blog/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
+from config import Config
 
 # instantiate the app
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # Modules
 CORS(app)
