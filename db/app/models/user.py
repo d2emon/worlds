2140db866b661aeff04527666a5b730c217a2b41
Dmitry Kutsenko <d2emonium@gmail.com>
2019-09-18 16:31:09 +0300

    Reset password

--- db/app/models/user.py
+++ db/app/models/user.py
@@ -1,7 +1,9 @@
-from app import db, login
+import jwt
+from app import app, db, login
 from datetime import datetime
 from flask_login import UserMixin
 from hashlib import md5
+from time import time
 from werkzeug.security import generate_password_hash, check_password_hash
 from .post import Post
 
@@ -45,6 +47,29 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {
+                'reset_password': self.user_id,
+                'exp': time() + expires_in,
+            },
+            app.config['SECRET_KEY'],
+            algorithm='HS256',
+        ).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(
+                token,
+                app.config['SECRET_KEY'],
+                algorithms=['HS256']
+            )['reset_password']
+        except jwt.exceptions.InvalidSignatureError:
+            return
+
+        return User.query.get(id)
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
--- db/app/routes.py
+++ db/app/routes.py
@@ -1,12 +1,13 @@
 import uuid
 from app import app, db
+from app.mail import send_password_reset_email
 from app.models import Post, User
 from datetime import datetime
 from flask import flash, jsonify, render_template, redirect, request, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from flask_wtf.csrf import generate_csrf
 from werkzeug.urls import url_parse
-from .forms import EditProfileForm, LoginForm, PostForm, RegistrationForm
+from .forms import EditProfileForm, LoginForm, PostForm, RegistrationForm, ResetPasswordForm, ResetPasswordRequestForm
 
 
 @app.before_request
@@ -210,6 +211,50 @@ def explore():
         posts=posts.items,
         prev_url=prev_url,
         next_url=next_url,
+    )
+
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+
+    return render_template(
+        'reset_password_request.html',
+        title="Reset Password",
+        form=form,
+    )
+
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('login'))
+
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+
+    return render_template(
+        'reset_password.html',
+        form=form,
     )
 
 
