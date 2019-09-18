bd669925b5c649c986f8346f007a285de3b3c8d2
Dmitry Kutsenko <d2emonium@gmail.com>
2019-09-18 11:18:51 +0300

    Profile

--- db/app/routes.py
+++ db/app/routes.py
@@ -1,11 +1,19 @@
 import uuid
 from app import app, db
 from app.models import User
+from datetime import datetime
 from flask import flash, jsonify, render_template, redirect, request, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from flask_wtf.csrf import generate_csrf
 from werkzeug.urls import url_parse
-from .forms import LoginForm, RegistrationForm
+from .forms import EditProfileForm, LoginForm, RegistrationForm
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route('/')
@@ -131,6 +139,42 @@ def register():
         form=form,
     )
 
+
+@app.route('/user/<username>')
+@login_required
+def user_profile(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #1'},
+    ]
+    return render_template(
+        'user.html',
+        user=user,
+        posts=posts,
+    )
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+
+    return render_template(
+        'edit_profile.html',
+        title="Edit Profile",
+        form=form,
+    )
 
 # API
 
