8fe8c19a084ce962aa7b0e26c680a6da902ef186
Dmitry Kutsenko <d2emonium@gmail.com>
2019-09-18 16:58:20 +0300

    Bootstrap
    (cherry picked from commit d754a78d1654dcce78590c53b342290e76a35c5a)

--- db/app/__init__.py
+++ db/app/__init__.py
@@ -1,5 +1,6 @@
 from config import Config
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_cors import CORS
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -11,10 +12,13 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 # Modules
+bootstrap = Bootstrap(app)
+
 CORS(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = "Пожалуйста, войдите, чтобы открыть эту страницу."
 
 mail = Mail(app)
 
